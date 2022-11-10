5b3b285230ea4f2e6ca4af2979b600ead383c83c
ropsutius <ropsutius@gmail.com>
2022-11-10 16:17:14 +0200

    Completed unicafe

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Header = ({ header }) => <h1>{header}</h1>;
+
+const Button = ({ handler, state, text }) => {
+  return <button onClick={() => handler(state + 1)}>{text}</button>;
+};
+
+const StatisticsLine = ({ value, text }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>no feedback given</p>;
+  } else {
+    return (
+      <>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticsLine value={good} text="good" />
+            <StatisticsLine value={neutral} text="neutral" />
+            <StatisticsLine value={bad} text="bad" />
+            <StatisticsLine value={good + neutral + bad} text="all" />
+            <StatisticsLine
+              value={(good - bad) / (good + neutral + bad)}
+              text="average"
+            />
+            <StatisticsLine
+              value={(good / (good + neutral + bad)) * 100}
+              text="positive"
+            />
+          </tbody>
+        </table>
+      </>
+    );
+  }
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const header = 'give feedback';
+
+  return (
+    <div>
+      <Header header={header} />
+      <Button handler={setGood} state={good} text="good" />
+      <Button handler={setNeutral} state={neutral} text="neutral" />
+      <Button handler={setBad} state={bad} text="bad" />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
 export default App;
