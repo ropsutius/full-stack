755b9e65c1768d03cddf5865744439dfde54d7c0
ropsutius <ropsutius@gmail.com>
2022-11-10 17:12:36 +0200

    Added anecdotes

---
+++ part-1/anecdotes/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Display = ({ anecdote, votes }) => {
+  return (
+    <>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </>
+  );
+};
+
+const TopAnecdote = ({ anecdotes, votes }) => {
+  const isEmpty = votes.every((vote) => vote === 0);
+  if (!isEmpty) {
+    let anecdote = votes.indexOf(Math.max(...votes));
+    return (
+      <>
+        <Header text="Anecdote with most votes" />
+        <Display anecdote={anecdotes[anecdote]} votes={votes[anecdote]} />
+      </>
+    );
+  }
+};
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  ];
+
+  const listOfVotes = new Array(anecdotes.length).fill(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(listOfVotes);
+
+  return (
+    <div>
+      <Header text="Anecdote of the day" />
+      <Display anecdote={anecdotes[selected]} votes={votes[selected]} />
+
+      <button
+        onClick={() => {
+          const copy = [...votes];
+          copy[selected]++;
+
+          setVotes(copy);
+        }}
+      >
+        vote
+      </button>
+
+      <button
+        onClick={() => {
+          let randomValue = Math.floor(Math.random() * anecdotes.length);
+          setSelected(randomValue);
+        }}
+      >
+        next anecdote
+      </button>
+      <TopAnecdote anecdotes={anecdotes} votes={votes} />
+    </div>
+  );
+};
+
+export default App;
