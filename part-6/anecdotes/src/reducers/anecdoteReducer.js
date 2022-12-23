de9a8d03f1841024502ed3b945b788c0a299ebf6
Roope Lehikoinen <ropsutius@gmail.com>
2022-12-23 13:30:32 +0200

    Added anecdote exercise

--- part-6/anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/anecdotes/src/reducers/anecdoteReducer.js
@@ -4,26 +4,47 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'ADD_ANECDOTE':
+      return state.concat({
+        content: action.data.anecdote,
+        id: getId(),
+        votes: 0,
+      });
+    case 'VOTE':
+      return state
+        .map((anecdote) =>
+          anecdote.id === action.data.id
+            ? { ...anecdote, votes: anecdote.votes + 1 }
+            : anecdote
+        )
+        .sort((a, b) => b.votes - a.votes);
+    default:
+      return state;
+  }
+};
 
-  return state
-}
+export const voteById = (id) => ({ type: 'VOTE', data: { id } });
 
-export default reducer
+export const addAnecdote = (anecdote) => ({
+  type: 'ADD_ANECDOTE',
+  data: { anecdote },
+});
+
+export default reducer;
